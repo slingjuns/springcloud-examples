45769aae00ce65067c4c1c4f6badff13441b5e3d
slingjun <73049845+slingjun@users.noreply.github.com>
2024-03-21 17:47:17 -0400

    add swagger-ui to emp/dept service

--- management/src/main/java/com/example/management/configs/SpringFoxConfig.java
+++ management/src/main/java/com/example/management/configs/SpringFoxConfig.java
@@ -9,13 +9,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SpringFoxConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
 }
